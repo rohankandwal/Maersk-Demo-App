55fd8c1133eae7cdbda1d816f0f5f7e4e7d08c1f
Rohan Kandwal <rohankandwal@Rohans-MacBook-Pro.local>
2022-03-07 14:21:27 +0530

    FeedScreen - base UI created

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -8,6 +8,8 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +19,82 @@ class _FeedScreenState extends State<FeedScreen> {
     );
   }
 
+  /// Gets the Appbar for the screen
   AppBar _getAppBar() {
-    return AppBar();
+    return AppBar(
+      title: const Text(
+        "Feed",
+      ),
+      centerTitle: true,
+    );
   }
 
+  /// Gets the floating action button which will redirect to create new screen
   Widget _getFloatingActionButton() {
-    return Container();
+    return FloatingActionButton(
+      onPressed: () {},
+      child: const Icon(Icons.add),
+    );
   }
 
+  /// Gets the main body for the app which contains a searchbar and a listview
   Widget _getBody() {
-    return Container();
+    return Stack(
+      children: [
+        Positioned(
+          child: _getSearchField(),
+          left: 0,
+          right: 0,
+          top: 0,
+        ),
+        Positioned(
+          child: _getFeedItems(),
+          left: 0,
+          right: 0,
+          top: 120,
+          bottom: 0,
+        )
+      ],
+    );
+  }
+
+  Widget _getSearchField() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      height: 120,
+      child: TextFormField(
+        controller: _searchController,
+        decoration: InputDecoration(
+          hintText: "Search",
+          border: _getBorder(Colors.black),
+          enabledBorder: _getBorder(Colors.black),
+          errorBorder: _getBorder(Colors.red),
+        ),
+      ),
+    );
+  }
+
+  InputBorder _getBorder(final Color color) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: color),
+    );
+  }
+
+  Widget _getFeedItems() {
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.red,
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            height: 10,
+          );
+        },
+        itemCount: 10);
   }
 }
